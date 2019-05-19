92096e260ce6c0ede59e6d544adefe8ad5cc0c59
Arthur Song <arth3rs0ng@gmail.com>
2019-05-19 12:15:42 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -39,8 +39,8 @@ class PetsController < ApplicationController
 
   patch '/pets/:id' do 
     @pet = Pet.find(params[:id])
-    if !params["owner"]["name"].empty?
-      @owner = Owner.create(name: params["owner"]["name"])
+    if !params["owner_name"].empty?
+      @owner = Owner.create(name: params["owner_name"])
       @owner.pets << @pet
       @owner.save
     else
